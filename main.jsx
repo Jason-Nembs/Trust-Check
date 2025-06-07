a41ffa94082e4032e5258559a30bbd67f8c0704a
Jason <jnembaware@gmail.com>
2025-06-07 22:51:43 +0300

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TrustScoreApp from "./TrustScoreApp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TrustScoreApp />
+  </React.StrictMode>
+);
